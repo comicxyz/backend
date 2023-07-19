065e05db94fa451b8012691a9f0c621830beb969
ComicXYZ <comicxyzproject@gmail.com>
2023-07-19 21:35:26 +0700

    set default interpreter to /bin/sh

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,42 +2,49 @@ module.exports = {
   apps: [{
     script: 'start.sh',
     name: "server",
-    args: 'dist/server.js'
+    interpreter: "/bin/sh"
   },
   {
     script: 'start.sh',
     name: "AddChapterToDbWorker",
     args: 'dist/workers/startAddChapterToDbWorker.js',
+    interpreter: "/bin/sh"
   },
   {
     script: 'start.sh',
     name: "CronScanDirWorker",
     args: 'dist/workers/startCronScanDirWorker.js',
+    interpreter: "/bin/sh"
   },
   {
     script: 'start.sh',
     name: "ExtractCoverWorker",
     args: 'dist/workers/startExtractCoverWorker.js',
+    interpreter: "/bin/sh"
   },
   {
     script: 'start.sh',
     name: "ZipWorker",
     args: 'dist/workers/startZipDownloadedChaptersWorker.js',
+    interpreter: "/bin/sh"
   },
   {
     script: 'start.sh',
     name: "WatchUrlWorker",
     args: 'dist/workers/startWatchUrlWorker.js',
+    interpreter: "/bin/sh"
   },
   {
     script: 'start.sh',
     name: "UnzipWorker",
     args: 'dist/workers/startUnzipChapterToTempDirWorker.js',
+    interpreter: "/bin/sh"
   },
   {
     script: 'start.sh',
     name: "DownloadWorker",
     args: 'dist/workers/startDownloadChaptersWorker.js',
+    interpreter: "/bin/sh"
   },
   ],
 };
