0564c82a5d4061f5317a799ad91469bf4bda80e3
ComicXYZ <comicxyzproject@gmail.com>
2023-07-19 23:55:07 +0700

    set default to noCoverOnly-deferred

--- src/config/configSchema.ts
+++ src/config/configSchema.ts
@@ -23,7 +23,7 @@ export const ScanDirConfigSchema = {
     type: 'string',
     enum: ['always', 'noCoverOnly', 'newOnly', 'never',
       'always-deferred', 'noCoverOnly-deferred', 'newOnly-deferred'],
-    default: 'never',
+    default: 'noCoverOnly-deferred',
   },
 };
 
