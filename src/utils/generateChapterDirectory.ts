492636c9ffbf0db7e3b2f8d51a804d42c8f827d1
ComicXYZ <comicxyzproject@gmail.com>
2023-07-19 22:50:02 +0700

    fixed generateChapterDirPath

--- src/utils/generateChapterDirectory.ts
+++ src/utils/generateChapterDirectory.ts
@@ -2,14 +2,12 @@ import { mkdir } from 'fs/promises';
 import { join } from 'path';
 
 export function generateChapterDirPath(
-  mangaId: string,
-  mangaSlug: string,
-  chapterId: string,
-  chapterSlug: string,
+  seriesTitle: string,
+  chapterTitle: string,
+  dir: string,
   subDir = 'Unsorted',
 ) {
-  const dir = process.env.MANGA_DIR || 'manga';
-  const path = join(dir, subDir, `${mangaId ? `${mangaId}-` : ''}${mangaSlug}`, `${chapterId ? `${chapterId}-` : ''}${mangaSlug} - ${chapterSlug}`);
+  const path = join(dir, subDir, seriesTitle, chapterTitle);
   return path;
 }
 
