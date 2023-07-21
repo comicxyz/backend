abbf2537a27951796ed95bae301f41e863e89b21
ComicXYZ <comicxyzproject@gmail.com>
2023-07-21 19:48:27 +0700

    add get title counts for alphabetically order

---
+++ src/routes/getTitleCounts.ts
@@ -0,0 +1,18 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+
+export default (app: FastifyInstance) => async (req: FastifyRequest<{
+  Params: { jobId: string } }>, res: FastifyReply) => {
+  const data = await app.objection.knex.raw(`
+    SELECT letter_group as letter,
+        COUNT(*) AS count
+        FROM (
+    SELECT
+        DISTINCT series_title,
+        letter_group
+    FROM
+        chapters) TB 
+    GROUP BY letter_group
+    ORDER BY letter_group ASC`);
+
+  return res.send(data.rows);
+};
