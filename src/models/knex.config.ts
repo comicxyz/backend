70ffdab069b0992b1b6d093d609f5eb94ca7d025
ComicXYZ <comicxyzproject@gmail.com>
2023-07-31 14:49:07 +0700

    add sqlite3 support

--- src/models/knex.config.ts
+++ src/models/knex.config.ts
@@ -1,15 +1,31 @@
 import Knex from 'knex';
 import { knexSnakeCaseMappers } from 'objection';
 import models from '../models/index.js';
+import logger from '../workers/logger.js';
+
+let client = process.env.DB_CLIENT || 'pg';
+
+if (client === 'sqlite3') {
+  client = 'better-sqlite3';
+}
+logger.info({ client }, 'Initializing database...');
 
 const connection = Knex({
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+  client,
+  connection: () => {
+    if (client === 'better-sqlite3') {
+      return {
+        filename: process.env.DB_FILENAME || './db.sqlite',
+      };
+    }
+
+    return {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+    };
   },
   ...knexSnakeCaseMappers({
     upperCase: false,
--- src/server.ts
+++ src/server.ts
@@ -23,15 +23,28 @@ async function start() {
   app.setNotFoundHandler(notFoundHandler);
   await app.register(replyWrapperPlugin);
 
+  let client = process.env.DB_CLIENT || 'pg';
+
+  if (client === 'sqlite3') {
+    client = 'better-sqlite3';
+  }
+
   await app.register(objectionjs, {
     knexConfig: {
-      client: 'pg',
-      connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
+      client,
+      connection: () => {
+        if (client === 'better-sqlite3') {
+          return {
+            filename: process.env.DB_FILENAME || './db.sqlite',
+          };
+        }
+        return {
+          host: process.env.DB_HOST || 'localhost',
+          port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+          user: process.env.DB_USER,
+          database: process.env.DB_NAME,
+          password: process.env.DB_PASSWORD,
+        };
       },
       // searchPath: ['public'],
       ...knexSnakeCaseMappers({
