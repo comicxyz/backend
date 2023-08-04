72c7ca844954157074173017131edc5332c0480d
ComicXYZ <comicxyzproject@gmail.com>
2023-08-04 23:11:24 +0700

    convert raw to querybuilder

--- src/routes/getTitleCounts.ts
+++ src/routes/getTitleCounts.ts
@@ -2,17 +2,20 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 export default (app: FastifyInstance) => async (req: FastifyRequest<{
   Params: { jobId: string } }>, res: FastifyReply) => {
-  const data = await app.objection.knex.raw(`
-    SELECT letter_group as letter,
-        COUNT(*) AS count
-        FROM (
-    SELECT
-        DISTINCT series_title,
-        letter_group
-    FROM
-        chapters) TB 
-    GROUP BY letter_group
-    ORDER BY letter_group ASC`);
+  const data = await app.objection.models.ChaptersModel.query()
+    .select(
+      app.objection.knex.raw('letter_group as letter'),
+      app.objection.knex.raw('COUNT(*) AS count'),
+    )
+    .from((qb) => {
+      qb.distinct('series_title', 'letter_group')
+        .from('chapters')
+        .as('TB');
+    })
+    .groupBy('letter_group')
+    .orderBy('letter_group', 'asc');
 
-  return res.send({ letterGroups: data.rows });
+  req.log.info({ data });
+
+  return res.send({ letterGroups: data });
 };
