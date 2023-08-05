5adab6446ce58d56b92a94d3788121b566e6dfc8
ComicXYZ <comicxyzproject@gmail.com>
2023-08-05 21:37:57 +0700

    add category

--- src/routes/browseComicOnServiceRoute.ts
+++ src/routes/browseComicOnServiceRoute.ts
@@ -1,11 +1,15 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
-import { GetComicListFunctionType } from '../@types/DownloaderInterfaces';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { GetComicListFunctionType, GetInfoFunctionType } from '../@types/DownloaderInterfaces';
 
-export default async (req: FastifyRequest<{
-  Params: { downloader: string },
-  Querystring: { page: number, search: string } }>, res: FastifyReply) => {
-  const { default: fn } = await import(`../downloaders/${req.params.downloader}/getComics.js`) as { default: GetComicListFunctionType };
-  const page = req.query.page || 1;
-  const search = req.query.search || '';
-  res.send({ comics: await fn({ page, search }) });
-};
+export default function getDownloadersRoute(app: FastifyInstance) {
+  return async (req: FastifyRequest<{
+    Params: { downloader: string },
+    Querystring: { page: number, search: string, category: string } }>, res: FastifyReply) => {
+    const { default: getInfo } = await import(`../../${app.config.DOWNLOADERS_DIR}/${req.params.downloader}/getInfo.js`) as { default: GetInfoFunctionType };
+    const { default: fn } = await import(`../downloaders/${req.params.downloader}/getComics.js`) as { default: GetComicListFunctionType };
+    const page = req.query.page || 1;
+    const search = req.query.search || '';
+    const category = req.query.category || '';
+    res.send({ info: getInfo(), comics: await fn({ page, search, category }) });
+  };
+}
