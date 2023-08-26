fbaf2b0515313858f45d804a779806eb8983a4f2
ComicXYZ <comicxyzproject@gmail.com>
2023-08-26 07:48:37 +0700

    catch error from axios when browsing comic

--- src/routes/browseComicOnServiceRoute.ts
+++ src/routes/browseComicOnServiceRoute.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { AxiosError } from 'axios';
 import { GetComicListFunctionType, GetInfoFunctionType } from '../@types/DownloaderInterfaces';
+import ServerError from '../plugins/ServerError.js';
 
 export default function getDownloadersRoute(app: FastifyInstance) {
   return async (req: FastifyRequest<{
@@ -10,6 +12,23 @@ export default function getDownloadersRoute(app: FastifyInstance) {
     const page = req.query.page || 1;
     const search = req.query.search || '';
     const category = req.query.category || '';
-    res.send({ info: getInfo(), comics: await fn({ page, search, category }) });
+    try {
+      const comics = await fn({ page, search, category });
+      res.send({ info: getInfo(), comics });
+    } catch (err) {
+      const axiosError = err as AxiosError;
+
+      if (axiosError.isAxiosError) {
+        throw new ServerError({
+          code: `ERR_BROWSE_COMICS_${axiosError.response?.status}` as Uppercase<string>,
+          message: `Cannot browse comics (Encounter HTTP Error ${axiosError.response?.status}${axiosError.response?.statusText && `: ${axiosError.response?.statusText}`})`,
+        });
+      }
+
+      throw new ServerError({
+        code: 'ERR_BROWSE_COMICS' as Uppercase<string>,
+        message: `Cannot browse comics (${axiosError.message})`,
+      });
+    }
   };
 }
